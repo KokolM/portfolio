4218009f90afc4ea153dd001a6fb69067fe2acf1
KokolM <matej.kokol@centrum.sk>
2024-03-13 20:16:42 +0100

    cleanup of template

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div class="flex flex-col gap-4">
+    <h1 class="text-3xl font-bold underline">Matej Kokol</h1>
+    <h2>kokol.matej0@gmail.com</h2>
+  </div>
+</template>
