12e72f7055e157102676b16a9db0250b9b39d86a
KokolM <matej.kokol@centrum.sk>
2024-03-14 20:43:27 +0100

    added Vercel SpeedInsights

--- app.vue
+++ app.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/next'
+</script>
+
 <template>
   <NuxtPage />
+  <SpeedInsights />
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
+import path from 'path'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  modules: ['nuxt-primevue'],
+  primevue: {
+    importPT: { from: path.resolve(__dirname, './presets/lara/') },
+  },
+  css: ['~/assets/css/main.css', 'primeicons/primeicons.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
