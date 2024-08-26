6010ff7dcae0b32d4bd25cd4478259412f8d84fa
Matej Kokol <matej.kokol@centrum.sk>
2024-08-26 16:30:34 +0200

    updated profile-image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,23 +2,29 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@primevue/nuxt-module', ['@nuxtjs/google-fonts', { families: { 'Saira Extra Condensed': [400,500, 600, 700, 800] } }]],
+
   primevue: {
     usePrimeVue: true,
     importTheme: { from: '@/themes/aura.js' },
   },
+
   css: ['~/assets/css/main.css', 'primeicons/primeicons.css', '@fortawesome/fontawesome-svg-core/styles.css', 'devicon/devicon.min.css'],
+
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  build: {
-    transpile: ['@fortawesome/vue-fontawesome'],
-  },
+
   // router: {
   //   options: {
   //     scrollBehaviorType: 'smooth'
   //   }
   // },
+  build: {
+    transpile: ['@fortawesome/vue-fontawesome'],
+  },
+
+  compatibilityDate: '2024-08-24',
 })
