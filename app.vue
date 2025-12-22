c2d6537d80933eb4983cbce2efb4fedaf150c38b
Matej Kokol <matej.kokol@centrum.sk>
2025-12-22 18:44:43 +0100

    added vercel analytics

--- app.vue
+++ app.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+import { Analytics } from '@vercel/analytics/nuxt'
 </script>
 
 <template>
@@ -8,5 +9,6 @@ import { SpeedInsights } from '@vercel/speed-insights/nuxt'
       <NuxtPage />
     </NuxtLayout>
     <SpeedInsights />
+    <Analytics />
   </div>
 </template>
