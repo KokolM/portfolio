e0e4a8baca625b3e68984bb46c6c6e48760ad0eb
Matej Kokol <matej.kokol@centrum.sk>
2026-02-26 00:08:13 +0100

    enhance analytics integration: add beforeSend event handler to control data sending

--- app.vue
+++ app.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 import { SpeedInsights } from '@vercel/speed-insights/nuxt'
-import { Analytics } from '@vercel/analytics/nuxt'
+import { Analytics, type BeforeSendEvent } from '@vercel/analytics/nuxt'
+
+const beforeSend = (event: BeforeSendEvent) => {
+    if (localStorage.getItem('va-disable') === 'true') {
+        return null
+    }
+    return event
+}
 </script>
 
 <template>
-  <div class="bg-white w-full text-text">
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-    <SpeedInsights />
-    <Analytics />
-  </div>
+    <div class="bg-white w-full text-text">
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+        <SpeedInsights />
+        <Analytics :before-send="beforeSend" />
+    </div>
 </template>
